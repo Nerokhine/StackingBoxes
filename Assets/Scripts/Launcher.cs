47ed07a38009d91d1590d4be4defa8b3be7a05e2
Nikita Erokhine <nikitaerokhine@American-Eagle.local>
2017-12-15 12:57:00 -0500

    arrow now spins and stops

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Launcher : MonoBehaviour {
 	GameObject powerBarMask;
 	GameObject launchingStation;
+	GameObject powerBar;
 	float launchForceX, launchForceY;
 	float initLaunchForceX, initLaunchForceY;
 	float maxLaunchForceX, maxLaunchForceY;
@@ -12,19 +13,24 @@ public class Launcher : MonoBehaviour {
 	bool increasingLaunchForce;
 	bool hasLaunched;
 	bool once;
+	bool once2, once3;
 	float delayBeforeCheckingCollision;
 	float delayBeforeCheckingCollisionCounter;
+	float motorSpeed;
+	JointMotor2D jointMotor;
 
 	// Use this for initialization
 	void Start () {
 		powerBarMask = GameObject.Find ("PowerBarMask");
 		launchingStation = GameObject.Find ("LaunchingStation");
+		powerBar = GameObject.Find ("PowerBar");
 		initLaunchForceX = 800f;
 		initLaunchForceY = 1600f;
 		maxLaunchForceX = 2000f;
 		maxLaunchForceY = 4000f;
 		rateLaunchForceX = 20f;
 		rateLaunchForceY = 40f;
+		motorSpeed = -50f;
 		delayBeforeCheckingCollision = 0.2f;
 		delayBeforeCheckingCollisionCounter = 0f;
 		increasingLaunchForce = true;
@@ -32,10 +38,39 @@ public class Launcher : MonoBehaviour {
 		launchForceX = initLaunchForceX;
 		launchForceY = initLaunchForceY;
 		once = true;
+		once2 = true;
+		once3 = true;
+		jointMotor = new JointMotor2D();
+		jointMotor.motorSpeed = motorSpeed;
+		jointMotor.maxMotorTorque = 10000f;
+		launchingStation.GetComponentInChildren<HingeJoint2D> ().motor = jointMotor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!Input.GetKey (KeyCode.Space)) {
+			if (powerBar.transform.localEulerAngles.z > 350f) {
+				if (once2) {
+					motorSpeed *= -1;
+					jointMotor.motorSpeed = motorSpeed;
+					launchingStation.GetComponentInChildren<HingeJoint2D> ().motor = jointMotor;
+					once3 = true;
+					once2 = false;
+				}
+			} else if (powerBar.transform.localEulerAngles.z < 280f) {
+				if (once3) {
+					motorSpeed *= -1;
+					jointMotor.motorSpeed = motorSpeed;
+					launchingStation.GetComponentInChildren<HingeJoint2D> ().motor = jointMotor;
+					once3 = false;
+					once2 = true;
+				}
+			}
+		} else {
+			jointMotor.motorSpeed = 0f;
+			launchingStation.GetComponentInChildren<HingeJoint2D> ().motor = jointMotor;
+		}
+
 		if (!hasLaunched) {
 			if (Input.GetKey (KeyCode.Space)) {
 				if (increasingLaunchForce) {
